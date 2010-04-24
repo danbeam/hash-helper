d63691a4aabb5642c3551dc011734156c67cbf85
Dan Beam <dan@danbeam.org>
2010-04-23 23:09:58 -0700

    Added some enhancements and documentation of each function

--- hashhelper.js
+++ hashhelper.js
@@ -1,47 +1,85 @@
-function HashHelper( delayOrFunc )
-{
-	this.delay = "number" === typeof delayOrFunc && window.NaN !== window.parseInt( delayOrFunc ) ? window.parseInt( delayOrFunc ) : 300;
-	this.oldHash = this.get( );
+/**
+ * @author	Dan Beam <dan@danbeam.org>
+ * @internal	A simple class to help those that want to #hash based navigation
+ * @param	mixed:delayOrFunc - either an Integer to denote the polling delay (in milliseconds), or an onChange callback
+ * @param	mixed:delayOrFunc - a function to be used as the onChange callback
+ * @return	an instance of HashHelper (usage:var hh = new HashHelper([...args...]);)
+ */
+
+function HashHelper (delayOrFunc) {
+	this.delay = "number" === typeof delayOrFunc && window.NaN !== window.parseInt(delayOrFunc) ? window.parseInt(delayOrFunc) : 300;
+	this.oldHash = this.get();
 	this.interval = null;
-	this.onChange = "function" === typeof delayOrFunc ? delayOrFunc : function( ){ };
+	this.onChange = "function" === typeof delayOrFunc ? delayOrFunc : function(){};
+	return this;
 }
 
-HashHelper.prototype.get = function( )
-{
-	return window.location.hash.slice( 1 );
+/**
+ * @internal	Returns the value if the text after the # in the URL (if it exists)
+ * @param	(none)
+ * @return	String:the value of the #hash (without #) or "" (if blank or non-existant)
+ */
+
+HashHelper.prototype.get = function () {
+	return window.location.hash.slice(1);
 };
 
-HashHelper.prototype.set = function( arg )
-{
-	window.location.hash = ( '#' !== arg[ 0 ] ? '#' : '' ) + arg;
+/**
+ * @internal	Sets the hash, changing the URL without reloading the page 
+ * @internal	In some browsers, this also creates a new history element (allowing Back/Forward navigation)
+ * @param	The new #hash to be set (in the URL0
+ * @return	(self)
+ */
+
+HashHelper.prototype.set = function (arg, callback) {
+	window.location.hash = ('#' !== arg[0] ? '#' : '') + arg;
+	if ('function' === typeof callback) { callback.call(this); }
+	return this;
 };
 
-HashHelper.prototype.check = function( )
-{
-	var     newHash = this.get( ),
+/**
+ * @internal	See if the current #hash is different from the stored value
+ * @internal	If a change is detected, the onChange event is trigger
+ * @param	(none)
+ * @return	Boolean:whether the hash has changed
+ */
+
+HashHelper.prototype.check = function () {
+	var     newHash = this.get(),
 		changed = newHash !== this.oldHash;
 
 	this.oldHash = newHash;
-
-	if( true === changed ){ this.onChange( newHash ); }
+	if (true === changed) { this.onChange( newHash ); return true; }
+	return false;
 };
 
-HashHelper.prototype.start = function( )
-{
-	this.check( );
+/**
+ * @internal	Start polling the #hash part of the location for changes
+ * @param	(none)
+ * @return	(self)
+ */
 
-	if( null === this.interval )
-	{
+HashHelper.prototype.start = function (callback) {
+	this.check();
+	if (null === this.interval) {
 		var that = this;
-		this.interval = window.setInterval( function( ){ that.check.call( that ); }, this.delay );
+		this.interval = window.setInterval(function (){ that.check.call(that); }, this.delay);
 	}
+	if ('function' === typeof callback) { callback.call(this); }
+	return this;
 };
 
-HashHelper.prototype.stop = function( )
-{
-	if( null !== this.interval )
-	{
-		window.clearInterval( this.interval );
+/**
+ * @internal	Stop polling the #hash part of the location for changes
+ * @param	(none)
+ * @return	(self)
+ */
+
+HashHelper.prototype.stop = function (callback) {
+	if (null !== this.interval) {
+		window.clearInterval(this.interval);
 		this.interval = null;
 	}
+	if ('function' === typeof callback) { callback.call(this); }
+	return this;
 };
